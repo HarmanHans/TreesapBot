9d441aa4537bfc765423c196293fdaf7b2e2dafa
HarmanHans <harmanhans56@gmail.com>
2023-09-24 15:05:34 -0700

    first push

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const tmi = require('tmi.js');
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+const client = new tmi.Client({
+	channels: [ 'lavenderfx' ],
+    identity: {
+		username: process.env.USERNAME,
+		password: process.env.TOKEN
+	},
+});
+
+const commands = {
+    
+    bf : {
+        res: () => 'Archer'
+    }
+}
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    if (self) return;
+
+    const [raw, command, argument] = message.match(regexpCommand);
+    const {res} = commands[command] || {};
+    if (typeof res === 'function') {
+        client.say(channel, res());
+    } else if (typeof res === 'string') {
+        client.say(channel, res);
+    }
+    
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
